ccac4ac493982b4b39f11a720360e85cde8b99ee
Jack Turkey <j77turk@outlook.com>
2025-05-09 09:45:48 -0400

    Overall Game Flow with print statements

--- main.py
+++ main.py
@@ -45,11 +45,32 @@ def deal(deck):
   return hands
 
 
-def checkPlay(topCard, playCard):
-    return (topCard.split(" ")[1] == playCard.split(" ")[1] or topCard.split(" ")[0] == playCard.split(" ")[0] or topCard.split(" ")[0] == "Wild" or playCard.split(" ")[0] == "Wild")
+def checkPlay(topCard, playCard, hand):
+  return ((topCard.split(" ")[1] == playCard.split(" ")[1] or topCard.split(" ")[0] == playCard.split(" ")[0] or topCard.split(" ")[0] == "Wild" or playCard.split(" ")[0] == "Wild") and (playCard in hand))
 
 def playGame():
-    deck = createDeck()
-    hands = deal(deck)
+  deck = createDeck()
+  hands = deal(deck)
+  playing = True
+  topCard = deck[0]
+  print("Top card is: " + topCard)
+  while playing:
+    for i in range(4):
+      print("Player " + str(i+1) + "'s hand: " + str(hands[i]))
+      if len(hands[i]) == 0:
+        print("Player " + str(i+1) + " wins!")
+        playing = False
+        break
+      else:
+        noValidMove = True
+        while noValidMove:
+          playCard = input("Player " + str(i+1) + ", play a card: ")
+          if checkPlay(topCard, playCard, hands[i]):
+            hands[i].remove(playCard)
+            deck.insert(0, playCard)
+            topCard = playCard
+            noValidMove = False
+          else:
+            print("Invalid move!")
 
 playGame()
